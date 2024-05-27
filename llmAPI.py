1bc5128801aa6f4ef684611c68189ef7a79f32af
berni <bernizapico@gmail.com>
2024-05-27 19:10:28 -0300

    added mangum and aws for host

--- llmAPI.py
+++ llmAPI.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Path, Query, HTTPException, status
 from pydantic import BaseModel
 from llm import get_recipes
+from mangum import Mangum
 
 class Ingredients(BaseModel):
     Ingredients_list: list
@@ -10,3 +11,5 @@ app = FastAPI()
 @app.post("/request-recipes")
 def request_recipes(ingredients: Ingredients):
     return get_recipes(ingredients)
+
+handler = Mangum(app)
